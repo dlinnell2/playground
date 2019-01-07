39ecaec09e1ca65fc230257e90a00462688a2aae
Donald Linnell <dlinnell@donalds-air.attlocal.net>
2019-01-06 21:25:28 -0600

    changing from library to direclty using maps

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
-import MapContainer from './components/MapContainer'
+import Map from './components/Map'
 
 class App extends Component {
 
   render() {
     return (
-      <div>
-        <MapContainer />
+      <div style = {{height: '100vh'}}>
+        <Map
+          id="myMap"
+          options={{
+            center: { lat: 41.0082, lng: 28.9784 },
+            zoom: 8
+          }}
+          onMapLoad={map => {
+            var marker = new window.google.maps.Marker({
+              position: { lat: 41.0082, lng: 28.9784 },
+              map: map,
+              title: 'Hello Istanbul!'
+            });
+          }}
+        />
       </div>
     );
   }
---
+++ client/src/components/Map.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+class Map extends Component {
+  constructor(props) {
+    super(props);
+    this.onScriptLoad = this.onScriptLoad.bind(this)
+  }
+
+  onScriptLoad() {
+    const map = new window.google.maps.Map(
+      document.getElementById(this.props.id),
+      this.props.options);
+    this.props.onMapLoad(map)
+  }
+
+  componentDidMount() {
+    if (!window.google) {
+      var s = document.createElement('script');
+      s.type = 'text/javascript';
+      s.src = `https://maps.google.com/maps/api/js?key=AIzaSyDo6tEEKB4ZkOL-DN7iFCHhwBwJahz7ax8`;
+      var x = document.getElementsByTagName('script')[0];
+      x.parentNode.insertBefore(s, x);
+      s.addEventListener('load', e => {
+        this.onScriptLoad()
+      })
+    } else {
+      this.onScriptLoad()
+    }
+  }
+
+  render() {
+    return (
+      <div style={{ width: '100%', height: '100%' }} id={this.props.id} />
+    );
+  }
+}
+
+export default Map
